e03a22433ead605817267c0745462b85f1a6a6d8
omjogani <om080jogani@gmail.com>
2026-01-27 16:59:49 +0530

    add: complexity for fun

--- react/incubyte-okr/src/App.tsx
+++ react/incubyte-okr/src/App.tsx
@@ -1,8 +1,31 @@
-function App() {
+import {useState} from "react";
 
-  return (
-    <p>hello</p>
-  )
+function App() {
+    const [counter, setCounter] = useState(0);
+    const [showDecrement, setShowDecrement] = useState(counter > 0);
+
+    function incrementCounter() {
+        setCounter((prev) => {
+            prev += 1;
+            setShowDecrement(prev > 0);
+            return prev;
+        });
+    }
+
+    function decrementCounter() {
+        setCounter(counter - 1);
+        console.log("Counter: ", counter);
+        setShowDecrement(counter > 1);
+        console.log("Should Show: ", showDecrement);
+    }
+
+    return (
+        <div>
+            <p>counter: {counter}</p>
+            <button onClick={incrementCounter}>Increment Counter</button>
+            {showDecrement ? <button onClick={decrementCounter}>Decrement Counter</button> : null}
+        </div>
+    );
 }
 
 export default App
