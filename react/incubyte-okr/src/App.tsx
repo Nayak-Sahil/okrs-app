98e2021b014752e675bafe26fb5dd8d8bab7188f
omjogani <om080jogani@gmail.com>
2026-01-28 10:06:55 +0530

    update: clean up

--- react/incubyte-okr/src/App.tsx
+++ react/incubyte-okr/src/App.tsx
@@ -2,21 +2,14 @@ import {useState} from "react";
 
 function App() {
     const [counter, setCounter] = useState(0);
-    const [showDecrement, setShowDecrement] = useState(counter > 0);
+    const showDecrement = counter > 0;
 
     function incrementCounter() {
-        setCounter((prev) => {
-            prev += 1;
-            setShowDecrement(prev > 0);
-            return prev;
-        });
+        setCounter(counter + 1);
     }
 
     function decrementCounter() {
         setCounter(counter - 1);
-        console.log("Counter: ", counter);
-        setShowDecrement(counter > 1);
-        console.log("Should Show: ", showDecrement);
     }
 
     return (
