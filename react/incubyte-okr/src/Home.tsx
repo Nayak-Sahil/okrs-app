5626cba23511dc28c2bc202fa6d74e4b0a18198e
Sahil Nayak <sahil.n@incubyte.co>
2026-01-31 08:43:07 +0530

    ENH: Integrate with local json-server db.json

--- react/incubyte-okr/src/Home.tsx
+++ react/incubyte-okr/src/Home.tsx
@@ -1,9 +1,21 @@
+import { useState, useEffect } from 'react';
 import { Modal } from './components/Modal.tsx';
 import OkrForm from './OkrForm.tsx';
 import { OkrList } from './components/OkrList.tsx';
 import type { OkrType } from './types/okr_types.ts';
 
 export function Home() {
+  const [okrs, setOkrs] = useState<OkrType[]>([]);
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_API_URL)
+      .then((response) => response.json())
+      .then((data) => setOkrs(data))
+      .catch((e) => {
+        console.error('Error fetching OKRs:', e);
+      });
+  }, []);
+
   return (
     <div className={'flex flex-col align-center gap-5 h-screen'}>
       <div className={'w-full flex justify-between p-5 border-b border-b-gray-200 shadow-md'}>
@@ -13,17 +25,7 @@ export function Home() {
         </Modal>
       </div>
       <div className={'w-full flex align-center justify-center'}>
-        <OkrList
-          okrs={[
-            {
-              objective: 'Learn React',
-              keyResults: [
-                { description: 'Typescript', progress: '34%', isCompleted: true },
-                { description: 'Vite', progress: '34%', isCompleted: false },
-              ],
-            } satisfies OkrType,
-          ]}
-        />
+        <OkrList okrs={okrs} />
       </div>
     </div>
   );
